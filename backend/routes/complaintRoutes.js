2a33a8caf1ce646689ad917527d21def21d91561
patibandasarvani <patibandasarvani@gmail.com>
2025-11-02 22:04:59 +0530

    Deleted uploads folder

--- backend/routes/complaintRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
-const upload = require("../middleware/upload");
-const { submitComplaint } = require("../controllers/complaintController");
-
-router.post("/", authMiddleware, upload.single("attachment"), submitComplaint);
-
-module.exports = router;
--- backend/routes/trackRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { trackComplaintById } = require("../controllers/trackController");
-
-// Public route to track complaint by ID (no login needed)
-router.get("/:complaintId", trackComplaintById);
-
-module.exports = router;
